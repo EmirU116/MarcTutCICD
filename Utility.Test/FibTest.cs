a0935bf564577a1fc081d63c4648db7edc029d18
Emir Ulusoy <emir_ulusoy_49@hotmail.com>
2024-08-06 18:40:25 +0200

    Setting up Unit Test with Fibonacci Recrusive function

---
+++ Utility.Test/FibTest.cs
@@ -0,0 +1,11 @@
+namespace Utility.Test;
+
+public class FibTest
+{
+
+    [Test]
+    public void Test1()
+    {
+        Utility.Fibonacci.Recrusive(5);
+    }
+}
---
+++ Utility/Fibonacci.cs
@@ -0,0 +1,10 @@
+﻿namespace Utility;
+
+public class Fibonacci
+{
+    public static int Recrusive(int n)
+    {
+        if (n < 2) return n;
+        return Recrusive(n - 2) + Recrusive(n - 1);
+    }
+}
