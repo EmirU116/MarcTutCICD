3d7fa58ce24c6239bb6d7a397d7c911281cf25e0
Emir Ulusoy <emir_ulusoy_49@hotmail.com>
2024-08-06 19:08:41 +0200

    fixed the code that was being broken

--- Utility/Fibonacci.cs
+++ Utility/Fibonacci.cs
@@ -4,7 +4,7 @@ public class Fibonacci
 {
     public static int Recrusive(int n)
     {
-        if (n < 1) return n;
+        if (n < 2) return n;
         return Recrusive(n - 2) + Recrusive(n - 1);
     }
 }
